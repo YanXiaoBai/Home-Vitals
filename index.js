7b937043e73f4d087f7c79ace854bd381bc9311c
YanXiaoBai <34643361+YanXiaoBai@users.noreply.github.com>
2018-01-06 14:33:00 +0800

    Logically completed
    This version has actualized the search mode, but still lacks improvement.

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
     var nowState = 0; //0时机动按钮为添加症状 1时机动按钮为保存症状
     $(".input_new").hide();
     $(".add_new").click(function() {
-        console.log("click");
         if (nowState == 1) {
+            document.getElementById("input_list").value = document.getElementById("input_list").value.toLowerCase();
             symptomList.push(document.getElementById("input_list").value);
             $(".input_new").before("<div class='now_symptom'>" + document.getElementById("input_list").value + "</div>")
             document.getElementById("input_list").value = "";
@@ -23,9 +23,7 @@ $(document).ready(function() {
     $(".submit").click(function() {
         $("#finding_page").show();
         $("#search_page").hide();
-        // for(var i=0;i<symptomList.length;i++)
-        // 	console.log(symptomList[i]);
-        
+
         function linkOfSymptom(numOfMatch, indexInJSON) {
             this.numOfMatch = numOfMatch;
             this.indexInJSON = indexInJSON;
@@ -35,23 +33,39 @@ $(document).ready(function() {
             return (-(a.numOfMatch - b.numOfMatch));
         }
         var listOfDisease;
-        var listOfCompare=new Array();
+        var listOfCompare = new Array();
         $.ajax({
             url: "data.json",
             async: false,
             success: function(data, status) {
-                listOfDisease = eval("(" + data + ")");
+                listOfDisease=data;
             }
         });
-        for(var i=0;i<listOfDisease.numOfDisease;i++)
-        {
-        	listOfCompare.push(new linkOfSymptom(0,i));
+        for (var i = 0; i < listOfDisease.numOfDisease; i++) {
+            listOfCompare.push(new linkOfSymptom(0, i));
         }
-        for(var i=0;i<symptomList.length;i++)
-        {
-        	for(var j=0;j<listOfDisease.numOfDisease;j++){
-        		
+        for (var i = 0; i < symptomList.length; i++) {
+            for (var j = 0; j < listOfDisease.numOfDisease; j++) {
+                for (var k = 0; k < listOfDisease.diseaseList[j].numOfSymptom; k++) {
+                    if (symptomList[i] == listOfDisease.diseaseList[j].symptom[k]) {
+                        listOfCompare[j].numOfMatch++;
+                    }
+                }
+            }
+        }
+        for(i=0;i<listOfCompare.length;i++){
+        	console.log("!")
+        	if(listOfCompare[i].numOfMatch==0){
+        		break;
         	}
+        	$(".rest").append("<div class='answer'><div class='name'>"+listOfDisease.diseaseList[listOfCompare[i].indexInJSON].name+"</div><div class='description'>Description:"+listOfDisease.diseaseList[listOfCompare[i].indexInJSON].description+"</div><div class='symptom'>Symptom:"+listOfDisease.diseaseList[listOfCompare[i].indexInJSON].symptom.toString()+"</div><div class='solution'>Solution:"+listOfDisease.diseaseList[listOfCompare[i].indexInJSON].solution+"</div></div>")
         }
+        // for (i = 0; i < listOfCompare.length; i++) {
+        //     console.log(listOfCompare[i].indexInJSON);
+        // }
+        // listOfCompare.sort(losCompare);
+        // for (i = 0; i < listOfCompare.length; i++) {
+        //     console.log("{"+listOfCompare[i].indexInJSON.toString()+" "+listOfCompare[i].numOfMatch.toString()+"}");
+        // }
     });
 });
